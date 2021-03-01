f775869f3d876fd90bf4cbf887b013b31de85400
Douglas <douglasgommez@gmail.com>
2021-03-01 15:23:26 -0300

    design UI and  code the app

--- app/src/main/java/com/odougle/imccalculator/MainActivity.kt
+++ app/src/main/java/com/odougle/imccalculator/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.odougle.imccalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    fun setListeners(){
+        btCalculate.setOnClickListener {
+            imcCalc(edtPeso.text.toString(),edtAltura.text.toString())
+        }
+    }
+
+    private fun imcCalc(peso: String, altura: String){
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+
+        if(peso != null &&  altura != null){
+            val imc = peso / (altura * altura)
+            txtTittle.text = "Seu IMC é ${imc}"
+            txtTittle.textSize = 24F
+        }
     }
 }
